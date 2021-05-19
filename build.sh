ba6a2479e119d87c2aec4f93dcc0456c0a0f2784
Attila Hammer <hammera@pickup.hu>
2021-05-19 11:44:38 +0200

    Add build scrypt

---
+++ build.sh
@@ -0,0 +1,3 @@
+#! /bin/sh
+systemd-nspawn
+ubuntu-defaults-image --flavor ubuntu-mate --components main,restricted,universe,multiverse --ppa belin/stable --keep-apt --keep-apt-components main,restricted,universe,multiverse --release bionic belin-mate-defaults-hu
