c92973ed26299e760b46b92fe4e4c4b2ba18ba22
Attila Hammer <hammera@pickup.hu>
2021-05-21 21:08:06 +0200

    Modify build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #! /bin/sh
-ubuntu-defaults-image --flavor ubuntu-mate --components main,restricted,universe,multiverse --ppa belin/stable --keep-apt --keep-apt-components main,restricted,universe,multiverse --release bionic --package belin-mate-defaults-hu --arch $1
+set -e
+export BUILDARCH=amd64
+export BUILDFLAVOUR=mate
+export BUILDLOG=quiet
+ubuntu-defaults-image --flavor ubuntu-mate --components main,restricted,universe,multiverse --ppa belin/stable --release bionic --package belin-mate-defaults-hu --arch $1
