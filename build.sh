df2635b5f087e200991483c3a1bfaaaef38f6839
Attila Hammer <hammera@pickup.hu>
2021-05-22 21:43:53 +0200

    Modify .travis.yml and build.sh file

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ set -e
 export BUILDARCH=amd64
 export BUILDFLAVOUR=mate
 export BUILDLOG=quiet
-ubuntu-defaults-image --flavor ubuntu-mate --components main,restricted,universe,multiverse --ppa belin/stable --release bionic --package belin-mate-defaults-hu --arch $1
+lb build
