bc2967c79b7faad70cc117af5043917855749634
Attila Hammer <hammera@pickup.hu>
2021-10-28 18:20:45 +0200

    Modify files

--- build.sh
+++ build.sh
@@ -1,3 +1,12 @@
 #! /bin/sh
-systemd-nspawn
-ubuntu-defaults-image --flavor ubuntu-mate --components main,restricted,universe,multiverse --ppa belin/stable --keep-apt --keep-apt-components main,restricted,universe,multiverse --release bionic --package belin-mate-defaults-hu
+lsb_release -d
+python3 scrypts/cdmanufacture
+apt-get -qq -y update && \
+apt-get -qq -y dist-upgrade -o dpkg::options::='--force-confold' && \
+apt-get -y -qq install btrfs-progs dmraid dmsetup dosfstools e2fsprogs ecryptfs-utils gpart jfsutils kpartx lvm2 mdadm mtools ntfs-3g reiser4progs reiserfsprogs xfsprogs xz-utils zfs-initramfs zfsutils zfsutils-linux && \
+apt-get -qq -y install grub2-common grub-pc-bin grub-efi-amd64-bin grub-efi-amd64-signed mtools xorriso && \
+apt-get -qq -y install `check-language-support -l en` && \
+apt-get -qq -y install `check-language-support -l hu` && \
+apt-get -qq update && apt-get -y -qq dist-upgrade && \
+apt-get autoremove --purge
+
