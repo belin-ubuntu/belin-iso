38702d8dd8c787f7d7b290ebfa81b3df7e267bf3
Attila Hammer <hammera@pickup.hu>
2021-11-29 18:47:07 +0100

    Fix a traceback error message

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ sudo apt-get -qq -y install `check-language-support -l en` && \
 sudo apt-get -qq -y install `check-language-support -l hu` && \
 sudo apt-get -qq update && apt-get -y -qq dist-upgrade && \
 sudo apt-get autoremove --purge
-sudo mkdir -p /home/runner/work/belin-iso/belin-iso/belin_iso;sudo python3 scrypts/cdmanufacture -a amd64 -c /home/runner/work/belin-iso/belin-iso/focal/mate/chroot -C /home/runner/work/belin-iso/belin-iso/focal/mate/cdstructure -e /home/runner/work/belin-iso/belin-iso/focal/mate/extra_commands.txt -i /home/runner/work/belin-iso/belin-iso/focal/mate/packages-install.txt -r /home/runner/work/belin-iso/belin-iso/focal/mate/packages-remove.txt -l "BeLin-7.0-mate" /home/runner/work/belin-iso/belin-iso/focal_mate.iso;sync;rm focal_mate.iso
+sudo mkdir -p /home/runner/work/belin-iso/belin-iso/belin_iso;sudo python3 scrypts/cdmanufacture -a amd64 -c /home/runner/work/belin-iso/belin-iso/focal/mate/chroot -C /home/runner/work/belin-iso/belin-iso/focal/mate/cdstructure -e /home/runner/work/belin-iso/belin-iso/focal/mate/extra_commands.txt -i /home/runner/work/belin-iso/belin-iso/focal/mate/packages-install.txt -r /home/runner/work/belin-iso/belin-iso/focal/mate/packages-remove.txt -l "BeLin-7.0-mate" /home/runner/work/belin-iso/belin-iso/focal_mate.iso;sync;rm focal_mate.iso;gzip /home/runner/work/belin-iso/belin-iso/belin_iso/belin-7.0-mate-amd64.iso
 
 
 
